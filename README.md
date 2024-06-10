6eae879bbd692913b2c30aa77797eaf873c98e05
Rodrigo Kassick <kassick@users.noreply.github.com>
2024-06-10 19:16:52 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,89 @@
+# Emacs Gitlab-LSP client for lsp-mode
+
+Gitlab Duo Code Assistant completions via [Gitlab LSP](https://gitlab.com/gitlab-org/editor-extensions/gitlab-lsp)
+
+![](./assets/example.gif)
+
+## Installing
+
+### Via Use-Package
+
+<details>
+
+``` elisp
+(use-package gitlab-lsp
+  :quelpa (gitlab-lsp :fetcher github
+                      :repo "kassick/gitlab-lsp.el"
+                      :branch "main"
+                      :files ("*.el")))
+```
+
+</details>
+
+### Spacemacs
+
+<details>
+
+```elisp
+
+;; In dotspacemacs/layers:
+
+(setq-default
+ ;; ...
+ dotspacemacs-additional-packages
+ '(
+   ;; ...
+   (gitlab-lsp :location (recipe
+                          :fetcher github
+                          :repo "kassick/gitlab-lsp.el"
+                          :files ("*.el")))
+   ;; ...
+   )
+ ;; ...
+ )
+
+
+;; In dotspacemacs/user-config:
+
+(require 'gitlab-lsp)
+```
+
+</details>
+
+
+## Setup
+
+### Install the LSP Server
+
+Run `lsp-install-server <RET> gitlab-lsp` to have `lsp-mode` download and install the server for you.
+
+You can also manually install the server anywhere in your `$PATH` or adjust `gitlab-lsp-executable` to have `lsp-mode` use the external command.
+
+### Generate an Access Token
+
+You must generate either a _Personal Access Token_ or an _OAuth Token_ to access your gitlab instance. The token must have the `api` scope.
+
+### Configure the LSP Client
+
+-   **Option 1: Secrets (GNOME Keyring or KDE Wallet)**: Simply open a file (of one of the supported modes) and you will be prompted for the GitLab instance _URL_ and _token_.
+
+    You can also `M-x gitlab-lsp-setup` at any time to create or update the token and URL.
+
+-   **Option 2: Emacs Custom Variables**: You can `M-x customize-group <RET> gitlab-lsp` and ajust the values of `gitlab-lsp-server-url` and `gitlab-lsp-token`.
+
+-   **Option 3**: You may set the environment variables `GITLAB_LSP_BASE_URL` and `GITLAB_LSP_TOKEN` and the client will pick them up.
+
+By default, `gitlab-lsp` will try to find the values in the environment variables, then custom variables, and finally secrets.
+
+If you have your URL and token stored via secrets and then either `(setenv "GITLAB_LSP_BASE_URL" "https://localhost:8080")` or `(setq gitlab-lsp-server-url "https://localhost:8080")`, then the client will use the URL from the environment and the token from secrets.
+
+### Usage
+
+Type your prompt and then either trigger company-mode completion (or wait on the timer). `company-capf` will call `lsp-mode` to provide completions, and then the configured gitlab instance will be contacted.
+
+## Known issues / limitations:
+
+1.  _Completion is slow_ -- yeah, the LSP server may take a while to complete. You may have to customize `lsp-response-timeout`.
+2.  _I only get no completions found_ -- make sure your lsp server has started correctly -- check the `*gitlab-lsp::stderr*` buffer and make sure that you see a `Token is valid` message.
+3.  _The single candidate appears as a very long line_ / _The completion list obscures the code snippet to be inserted_-- That's because `company-mode` is using the same UI for candidates and standard completions. This is a _frontend_ issue, to be solved elsewhere.
+4.  _How can I manually trigger only gitlab-lsp_ -- No idea still, working on it...
---
+++ gitlab-lsp.el
@@ -0,0 +1,318 @@
+;;; gitlab-lsp.el --- lsp-mode client  gitlab-lsp  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2024 Rodrigo Virote Kassick
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;; Author: Rodrigo Virote Kassick <kassick@gmail.com>
+;; Version: 0.1
+;; Package-Requires: (lsp-mode secrets s compile dash cl-lib request)
+;; Keywords: lsp-mode, generative-ai, code-assistant
+;; URL: https://github.com/kassick/gitlab-lsp.el
+
+;; Commentary:
+
+;; LSP client for gitlab-lsp -- https://gitlab.com/gitlab-org/editor-extensions/gitlab-lsp
+
+;; Code:
+
+(require 'lsp-mode)
+(require 'cl-lib)
+(require 'secrets)
+(require 's)
+(require 'compile)
+(require 'dash)
+(require 'request)
+
+(cl-defun lsp--npm-custom-registry-dependency-install (callback error-callback &key package registry-name registry-url &allow-other-keys)
+  "Same as lsp--npm-dependency-install, but accepts a `:registry-url' keyword to use as private registry when installing."
+  (if-let ((npm-binary (executable-find "npm")))
+      (let* ((registry-name (or registry-name "custom-registry"))
+             (process-environment (append (list (format "npm_config_@%s:registry=%s" registry-name registry-url))
+                                          process-environment))
+             (package-with-registry (format "@%s/%s" registry-name package))
+             (package-install-prefix (f-join lsp-server-install-dir "npm" package)))
+        ;; Explicitly `make-directory' to work around NPM bug in
+        ;; versions 7.0.0 through 7.4.1. See
+        ;; https://github.com/emacs-lsp/lsp-mode/issues/2364 for
+        ;; discussion.
+        (make-directory (f-join package-install-prefix "lib") 'parents)
+        (lsp-async-start-process (lambda ()
+                                   (if (string-empty-p
+                                        (string-trim (shell-command-to-string
+                                                      (mapconcat #'shell-quote-argument (list npm-binary "view" package-with-registry "peerDependencies") " "))))
+                                       (funcall callback)
+                                     (let ((default-directory (f-dirname (car (last (directory-files-recursively package-install-prefix "package.json")))))
+                                           (process-environment (append '("npm_config_yes=true") process-environment))) ;; Disable prompting for older versions of npx
+                                       (when (f-dir-p default-directory)
+                                         (lsp-async-start-process callback
+                                                                  error-callback
+                                                                  (executable-find "npx")
+                                                                  "npm-install-peers")))))
+                                 error-callback
+                                 npm-binary
+                                 "-g"
+                                 "--prefix"
+                                 package-install-prefix
+                                 "install"
+                                 package-with-registry
+                                 ))
+    (lsp-log "Unable to install %s via `npm' from %s because it is not present" package registry-url)
+    nil))
+
+(plist-put lsp-deps-providers
+           :npm-with-registry
+           (list :path #'lsp--npm-dependency-path
+                 :install #'lsp--npm-custom-registry-dependency-install))
+
+
+(lsp-dependency 'gitlab-lsp
+                '(:system "gitlab-lsp")
+                '(:npm-with-registry :package "gitlab-lsp"
+                                     :registry-name "gitlab-org"
+                                     :registry-url "https://gitlab.com/api/v4/packages/npm/"
+                                     :path "gitlab-lsp"))
+
+(lsp-register-custom-settings
+ '(
+   ;; TODO: add these as custom variables
+   ("gitlab-lsp.logLevel" "debug")
+   ("gitlab-lsp.telemetry.enabled" json-false)
+   ))
+
+(defgroup gitlab-lsp ()
+  "Gitlab-lsp configuration"
+  ;; :group 'lsp-mode
+  :tag "Gitlab LSP"
+  :link '(url-link "https://gitlab.com/gitlab-org/editor-extensions/gitlab-lsp"))
+
+(defcustom gitlab-lsp-langserver-command-args '("--stdio")
+  "Command to start gitlab-langserver."
+  :type '(repeat string)
+  :group 'gitlab-lsp)
+
+(defcustom gitlab-lsp-server-url nil
+  "The gitlab server instance used by gitlab-lsp"
+  :type '(choice (const :tag "Undefined" nil) string)
+  :group 'gitlab-lsp)
+
+(defcustom gitlab-lsp-token nil
+  "The token (personal-access-token or OAUTH) used when contacting
+the gitlab instance defined in gitlab-lsp-server-url."
+  :type '(choice (const :tag "Undefined" nil) string)
+  :group 'gitlab-lsp)
+
+(defcustom gitlab-lsp-executable "gitlab-lsp"
+  "The system-wise executable of gitlab-lsp.
+When this executable is not found, you can stil use
+lsp-install-server to fetch an emacs-local version of the LSP."
+  :type 'string
+  :group 'gitlab-lsp)
+
+(defcustom gitlab-lsp-major-modes '(python-mode python-ts-mode)
+  "The major modes for which gitlab-lsp should be used"
+  :type '(repeat symbol)
+  :group 'gitlab-lsp)
+
+
+(defconst gitlab-lsp-secrets-item-key "emacs/gitlab-lsp/global")
+
+;; TODO: fix weidness
+;; If I let the url be empty/null, then interactively will always prompt
+;; because the variable will have null baseurl and interactivelly will see that the
+;; setup is not complete ...
+
+(defun gitlab-lsp--locate-config-with-env ()
+  (cons (getenv "GITLAB_LSP_BASE_URL") (getenv "GITLAB_LSP_TOKEN")))
+
+(cl-defun gitlab-lsp--locate-config-with-variables (&key &allow-other-keys)
+  (cons gitlab-lsp-server-url gitlab-lsp-token))
+
+
+(cl-defun gitlab-lsp--locate-config-with-secrets (&key &allow-other-keys)
+  (-when-let (sattr (secrets-get-attributes "login" gitlab-lsp-secrets-item-key))
+    (let ((baseUrl (alist-get :baseUrl sattr))
+          (token (secrets-get-secret "login" gitlab-lsp-secrets-item-key)))
+      (cons baseUrl token))))
+
+(defun gitlab-lsp--validate-token (baseUrl token)
+  "The token must have the API scope"
+  (let* ((response (request (concat baseUrl "/api/v4/personal_access_tokens/self")
+                     :headers `(("Authorization" . ,(concat "Bearer " token)))
+                     :sync t
+                     :parser 'json-read))
+         (response-data (request-response-data response)))
+
+    (cond
+     ((/= 200 (request-response-status-code response))
+      `(nil . ,(alist-get 'message response-data)))
+
+     ((not (seq-contains (alist-get 'scopes response-data) "api"))
+      '(nil . "token does not contain the api scope"))
+
+     (t '(t nil)))))
+
+(cl-defun gitlab-lsp--locate-config-interactively (&key force-store &allow-other-keys)
+  (let ((baseUrl (string-trim (read-string "Gitlab Base URL (empty for default): ")))
+        (token (read-passwd "Token (PAT or OAUTH): "))
+        (store-values (or force-store (yes-or-no-p "Remember secret?"))))
+
+    (-let* ((url (if (length= baseUrl 0)
+                     "https://gitlab.com"
+                   baseUrl))
+            ((valid . err) (gitlab-lsp--validate-token url token)))
+      (unless valid
+        (error (format "Invalid Token: %s" err))))
+
+    (when store-values
+      ;; item key does not need to be unique, but we want a single entry here
+      (when (secrets-get-attributes "login" gitlab-lsp-secrets-item-key)
+        (secrets-delete-item "login" gitlab-lsp-secrets-item-key))
+
+      (secrets-create-item "login" gitlab-lsp-secrets-item-key token :baseUrl baseUrl))
+
+    (cons baseUrl token)))
+
+
+(defcustom gitlab-lsp-locate-token-fn-list
+  '(gitlab-lsp--locate-config-with-env
+    gitlab-lsp--locate-config-with-variables
+    gitlab-lsp--locate-config-with-secrets
+    gitlab-lsp--locate-config-interactively)
+  "List of functions used to locate values for the server baseUrl and Token"
+  :type '(repeat function)
+  :group 'gitlab-lsp)
+
+(defun gitlab-lsp-locate-config ()
+  "Returns (baseUrl . token) using the functions in gitlab-lsp-locate-token-fn-list.
+
+When any of the functions returns a non-nil baseUrl, this value
+will be used. Ditto for token. When both values are non nil, the
+function returns.
+
+This means that you can the token `xpto' for url
+`https://internal.server/' stored in secrets, but if you setenv
+GITLAB_LSP_BASE_URL for emacs, it will ignore the value obtained
+via secrets, as long as gitlab-lsp--locate-config-with-env
+appears before gitlab-lsp--locate-config-with-secrets.
+"
+  (let (baseUrl token)
+    (catch 'break
+      (dolist (fn gitlab-lsp-locate-token-fn-list)
+        (-let (((url . tk) (apply fn '())))
+
+          (when (and (not baseUrl) url)
+            (setq baseUrl url))
+
+          (when (and (not token ) tk)
+            (setq token tk))
+
+          (when (and baseUrl token)
+            (throw 'break nil)))))
+
+    (cons baseUrl token)))
+
+(defun gitlab-lsp-token-check-callback (workspace result &rest args)
+  (let ((message (ht-get result "message")))
+    (display-warning 'gitlab-lsp message :error)
+    (lsp--error message)))
+
+(defun kzk/around-lsp--start-workspace (fn &rest args)
+  "Ensure we do not use the build time in the client id"
+
+  ;; Gitlab LSP will use the client id as part of the headers -- and headers can not contain newlines and stuff
+  (let ((emacs-build-time nil))
+    (apply fn args)))
+
+(advice-add 'lsp--start-workspace :around #'kzk/around-lsp--start-workspace)
+
+
+(defun gitlab-lsp--server-initialization-options ()
+  (list :settings (list :logLevel "info"
+                        :version "0.1.0"
+                        :clientType "emacs"
+                        :clientVersion (symbol-value 'emacs-version))))
+
+(defun gitlab-lsp--server-initialized-fn (workspace)
+  (-let* (
+          ;; in emacs, the configuration is stored under `gitlab-lsp.key' but
+          ;; gitlab-lsp does not handle the prefix, so we must drop the
+          ;; prefix...
+          (config-ht (ht-get (lsp-configuration-section "gitlab-lsp")
+                             "gitlab-lsp"))
+          ((baseUrl . token) (gitlab-lsp-locate-config)))
+
+    ;; Empty string -- let the server use whatever default it wants
+    (when (length> (string-trim (or baseUrl "")) 0)
+      (ht-set config-ht "baseUrl" baseUrl))
+
+    (when token
+      (ht-set config-ht "token" token))
+
+    (with-lsp-workspace workspace
+      (lsp--set-configuration config-ht))))
+
+;; Server installed by emacs
+(lsp-register-client
+ (make-lsp-client
+  :new-connection (lsp-stdio-connection (lambda ()
+                                          (cons
+                                           (lsp-package-path 'gitlab-lsp)
+                                           gitlab-lsp-langserver-command-args)))
+  :activation-fn (lambda (_ mode)
+                   (member mode gitlab-lsp-major-modes))
+  :server-id 'gitlab-lsp
+  :multi-root nil
+  :priority -2
+  :add-on? t
+  :completion-in-comments? t
+  :initialization-options #'gitlab-lsp--server-initialization-options
+  :initialized-fn #'gitlab-lsp--server-initialized-fn
+  :download-server-fn (lambda (_client callback error-callback _update?)
+                        (lsp-package-ensure 'gitlab-lsp callback error-callback))
+  :notification-handlers (lsp-ht ("$/gitlab/token/check" 'gitlab-lsp-token-check-callback))))
+
+;; Server found in PATH
+(lsp-register-client
+ (make-lsp-client
+  :new-connection (lsp-stdio-connection (lambda ()
+                                          (cons
+                                           (executable-find gitlab-lsp-executable)
+                                           gitlab-lsp-langserver-command-args)))
+  :activation-fn (lambda (_ mode)
+                   (member mode gitlab-lsp-major-modes))
+  :server-id 'gitlab-lsp-remote
+  :multi-root nil
+  :priority -2
+  :add-on? t
+  :completion-in-comments? t
+  :initialization-options #'gitlab-lsp--server-initialization-options
+  :initialized-fn #'gitlab-lsp--server-initialized-fn
+  :download-server-fn (lambda (_client callback error-callback _update?)
+                        (lsp-package-ensure 'gitlab-lsp callback error-callback))
+  :notification-handlers (lsp-ht ("$/gitlab/token/check" 'gitlab-lsp-token-check-callback))))
+
+
+;;;###autoload
+(defun gitlab-lsp-setup ()
+  "Configures the access token for gitlab-lsp"
+  (interactive)
+
+  (gitlab-lsp--locate-config-interactively :force-store t)
+
+  nil)
+
+(provide 'gitlab-lsp)
